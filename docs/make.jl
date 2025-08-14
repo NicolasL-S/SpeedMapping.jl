a4c298b04e11cb6ed26ee8e4c9b93c95e043a6a8
Nicolas Lepage-Saucier <nicolas.lepagesaucier@gmail.com>
2025-08-14 16:41:41 -0400

    Major changes to the doc

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,19 @@
 push!(LOAD_PATH,"../src/")
 using SpeedMapping
 using Documenter
+using Literate
+
+cd(@__DIR__)
+Literate.markdown("./src/tutorial.jl")
 makedocs(
          sitename = "SpeedMapping.jl",
          modules  = [SpeedMapping],
          pages=[
-                "Home" => "index.md"
+                "Introduction" => "index.md",
+                "Tutorial" => "tutorial.md",
+                "API" => "api.md"
                ])
-deploydocs(;
-    repo="github.com/NicolasL-S/SpeedMapping.jl.git",
+makedocs(;pages, modules = [SpeedMapping])
+#deploydocs(;
+#    repo="github.com/NicolasL-S/SpeedMapping.jl.git",
 )
