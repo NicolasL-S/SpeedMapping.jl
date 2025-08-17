ac00444e12cb0a2c72d0dd2205498700b5e0e314
Documenter.jl <documenter@juliadocs.github.io>
2025-08-17 04:29:42 +0000

    build based on 7a95774

--- v0.4.0/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#SpeedMapping.jl","page":"Home","title":"SpeedMapping.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"speedmapping","category":"page"}]
-}
--- versions.js
+++ versions.js
@@ -4,6 +4,7 @@ var DOC_VERSIONS = [
   "v0.3",
   "v0.2",
   "v0.1",
+  "dev",
 ];
-var DOCUMENTER_NEWEST = "v0.4.0";
+var DOCUMENTER_NEWEST = "v0.4";
 var DOCUMENTER_STABLE = "stable";
